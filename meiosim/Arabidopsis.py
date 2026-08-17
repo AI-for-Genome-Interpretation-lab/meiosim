dcabd5837a8cab0789a1c7b2ba72d433eb5fe930
Almarch <alexandre.marchal@hotmail.com>
2026-08-17 18:30:18 +0200

    add vernalization longitudinal data

--- meiosim/Arabidopsis.py
+++ meiosim/Arabidopsis.py
@@ -81,14 +81,30 @@ class Arabidopsis(Population):
         )
 
         # ### Phenotypes
-        # Grimm et al. 2017 - easyGWAS: A Cloud-Based Platform for Comparing the Results of Genome-Wide Association Studies
+        # Grimm et al. 2017 - easyGWAS: A Cloud-Based Platform for Comparing the
+        # Results of Genome-Wide Association Studies
         # https://arapheno.1001genomes.org/study/38/
 
         pheno = pd.read_csv(os.path.join(_data_dir, "all_phenotypes_from_easyGWAS.csv"))
+
+        # ### Vernalization response
+        # Flowering time measured after 0, 2, 4 and 8 weeks of vernalization,
+        # as accession means in days from germination to first flower. Plants
+        # that had not flowered by the end of the experiment are recorded at the
+        # upper bound and kept on the continuous scale.
+        #
+        # Atwell et al. 2010 - Genome-wide association study of 107 phenotypes
+        # in Arabidopsis thaliana inbred lines
+        # https://arapheno.1001genomes.org/study/12/
+
+        vernalization = pd.read_csv(
+            os.path.join(_data_dir, "vernalization_flowering_time.csv")
+        )
+
+        pheno = pheno.merge(vernalization, on="accession_id", how="outer")
         pheno["individual"] = pheno["accession_id"].astype(str)
+
         self.phenotypes = pheno
-
-
 
 
         
